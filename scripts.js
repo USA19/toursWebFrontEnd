4c3b03b77cda95e7a5499e535173d16b630cef51
Usama Sarfraz <muhamedusama648@gmail.com>
2020-10-02 20:56:31 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+//start
+
+
+document.querySelector('.open').addEventListener('click',()=>{
+    document.querySelector('.nav-list').classList.add('active');
+});
+
+
+document.querySelector('.close').addEventListener('click',()=>{
+    document.querySelector('.nav-list').classList.remove('active');
+});
